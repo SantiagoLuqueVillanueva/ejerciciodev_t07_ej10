5d0bad8e30aaeeabb4209e1770303b781abfa11c
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-07 12:40:56 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,36 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] main = new int[20];
+
+        System.out.println("Array original:");
+
+        for (int i = 0; i < main.length; i++) {
+            main[i] = (int)(Math.random()*101);
+            System.out.print(main[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println("Array con los pares al principio:");
+
+        int[] end = new int[20];
+        int contador = 0;
+
+        for (int i = 0; i < end.length; i++) {
+            if (main[i]%2==0) {
+                end[contador] = main[i];
+                contador++;
+            }
+        }
+
+        for (int i = 0; i < main.length; i++) {
+            if (main[i]%2!=0) {
+                end[contador] = main[i];
+                contador++;
+            }
+        }
+
+        for (int i = 0; i < end.length; i++) {
+            System.out.print(end[i] + " ");
+        }
     }
 }
